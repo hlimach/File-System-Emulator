4a47caf5369e0c6862dc640f5cbe4f4c2fd7296b
hlimach <hramzan.bscs18seecs@seecs.edu.pk>
2020-11-25 20:24:12 +0500

    Finalized main switch statement

--- main.cpp
+++ main.cpp
@@ -19,27 +19,73 @@ struct memory {
     unsigned sectorAdd: 8; //8 bits unsigned value - 256 bytes per sector
 };
 
-class FileHead {
-    string filename;
-    int size;
-    char* directory;
-};
-
 char* getSector(int x) {
     char* sectorNo;
     sectorNo = (char*) start + (SECTORSIZE * x);
     return sectorNo;
 }
 
-void Create(string filename);
-void Delete(string filename);
-void Open(string filename);
-void Close(string filename);
-void List();
-void Write(string filename, char mode);
-void Read(string filename);
-void Read(string filename, int startFrom);
+string setFilename() {
+    string filename;
+    
+    cout << "Enter File name: ";
+    cin >> filename;
+    
+    return filename;
+}
 
+void create() {
+    setFilename();
+}
+
+void deleteFile() {
+    setFilename();
+}
+
+void open() {
+    setFilename();
+}
+
+void close() {
+    setFilename();
+}
+
+void list () {
+    setFilename();
+}
+
+void write() {
+    char mode;
+    
+    cout << "Enter mode: ";
+    cin >> mode;
+    
+    while(mode != 'a' && mode != 'w') {
+        cout << "Enter a valid mode!" << endl;
+        cout << "Enter mode: ";
+        cin >> mode;
+    }
+    
+    setFilename();
+    
+}
+void read () {
+    setFilename();
+}
+
+void read (int startFrom) {
+    setFilename();
+}
+
+
+bool isNumber(string s)
+{
+    for (int i = 0; i < s.length(); i++)
+        if (isdigit(s[i]) == false)
+            return false;
+ 
+    return true;
+}
 
 int main(int argc, const char * argv[]) {
     
@@ -47,67 +93,67 @@ int main(int argc, const char * argv[]) {
 
     start = (char*) malloc(16777216);
     
-    
-    while(true) {
+    bool loop = true;
+    while (loop) {
         int command, readByte;
-        string filename;
-        char writeMode;
+        string startFrom;
 
         cout << "Please enter one of the following command integer:" << endl;
-        cout << "1: Create(filename) \t\tCreate a text file of name <filename>" << endl;
-        cout << "2: Delete(filename) \t\tDelete a text file of name <filename>" << endl;
-        cout << "3: Open(filename) \t\tOpen a text file of name <filename>" << endl;
-        cout << "4: Close(filename) \t\tClose an opened text file of name <filename>" << endl;
-        cout << "5: List files \t\t\tList all the files in the current directory" << endl;
-        cout << "6: Read(filename) \t\tRead a text file of name <filename> from start" << endl;
+        cout << "1: Create(filename) \t\t\tCreate a text file of name <filename>" << endl;
+        cout << "2: Delete(filename) \t\t\tDelete a text file of name <filename>" << endl;
+        cout << "3: Open(filename) \t\t\t\tOpen a text file of name <filename>" << endl;
+        cout << "4: Close(filename) \t\t\t\tClose an opened text file of name <filename>" << endl;
+        cout << "5: List files \t\t\t\t\tList all the files in the current directory" << endl;
+        cout << "6: Read(filename) \t\t\t\tRead a text file of name <filename> from start" << endl;
         cout << "7: Read(filename, startFrom) \tRead a text file of name <filename> from the byte number given by <startFrom>" << endl;
-        cout << "8: Write(filename,mode) \t\tWrite to text file of name <filename> with the given mode\n\t\t\t\t\tMode: a (append), w (write)" << endl;
+        cout << "8: Write(filename,mode) \t\tWrite to text file of name <filename> with the given mode\n\t\t\t\t\t\t\t\tMode: a (append), w (write)" << endl;
         cout << "9: End Program" << endl;
-        
+        cout << "\nEnter command: ";
         cin >> command;
         
         
         switch (command) {
         case(1):
-            cout << "\nEnter File name: ";
-            cin >> filename;
-            break;
+                create();
+                break;
         case(2):
-            cout << "\nEnter File name: ";
-            cin >> filename;
-            break;
+                deleteFile();
+                break;
         case(3):
-            cout << "\nEnter File name: ";
-            cin >> filename;
-            break;
+                open();
+                break;
         case(4):
-            cout << "\nEnter File name: ";
-            cin >> filename;
-            break;
+                close();
+                break;
         case(5):
-            cout << "These are the files: \n";
-            break;
+                list();
+                break;
         case(6):
-            cout << "\nEnter File name: ";
-            cin >> filename;
-            cout << "\nEnter write mode: ";
-            cin >> writeMode;
-            break;
+                read();
+                break;
         case(7):
-            cout << "\nEnter File name: ";
-            cin >> filename;
-            break;
+                while(true) {
+                    cout << "Enter starting byte number: ";
+                    cin >> startFrom;
+                    
+                    if (isNumber(startFrom)) {
+                        read(stoi(startFrom));
+                        break;
+                    }
+                    else
+                        cout << "Enter a valid byte number!" << endl;
+                }
+                
+                break;
         case(8):
-            cout << "\nEnter File name: ";
-            cin >> filename;
-            cout << "\nEnter start byte: ";
-            cin >> readByte;
-            break;
+                write();
+                break;
         case(9):
-            break;
+                loop = false;
+                break;
         default:
-            cout << "Please enter a valid integer command!" << endl;
-            break;
+                cout << "Please enter a valid integer command!" << endl;
+                break;
         }
     }
 
